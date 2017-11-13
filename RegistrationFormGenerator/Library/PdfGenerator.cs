d6a7a3967efba9a5e3d12e79c2170281f2556d99
S. M. Abrar Jahin <abrarjahin@live.com>
2017-11-13 16:31:30 +0600

    Pdf Generating, but Unicode is not saving

--- RegistrationFormGenerator/Library/PdfGenerator.cs
+++ RegistrationFormGenerator/Library/PdfGenerator.cs
@@ -1,7 +1,6 @@
 ﻿using HtmlAgilityPack;
 using System.IO;
 using System.Windows;
-using System;
 using PdfSharp;
 using PdfSharp.Pdf;
 using TheArtOfDev.HtmlRenderer.PdfSharp;
@@ -10,9 +9,9 @@ namespace RegistrationFormGenerator.Library
 {
     class ExcelPdfGenerator
     {
-        internal static bool GeneratePdf(ExcelDataRow data, string outputFolderLocation)
+        internal static bool GeneratePdf(ExcelDataRow data, string imageFolderLocation, string outputFolderLocation)
         {
-            string htmlFileLocation = GenerateHtml(data, outputFolderLocation);
+            string htmlFileLocation = GenerateHtml(data,imageFolderLocation, outputFolderLocation);
             MessageBox.Show(data.Serial);
             return GeneratePdf(htmlFileLocation, @"D:\path.pdf");
         }
@@ -21,11 +20,13 @@ namespace RegistrationFormGenerator.Library
         {
             string html = File.ReadAllText(htmlFileLocation);
             PdfDocument pdf = PdfGenerator.GeneratePdf(html, PageSize.A4);
+            //XPdfFontOptions options = new XPdfFontOptions(PdfFontEncoding.Unicode, PdfFontEmbedding.Always);
             pdf.Save(outputPdflocation);
+            //should use this to show unicode - http://www.codescratcher.com/asp-net/display-unicode-characters-in-converting-html-to-pdf/
             return true;
         }
 
-        private static string GenerateHtml(ExcelDataRow data, string outputFolderLocation)
+        private static string GenerateHtml(ExcelDataRow data, string imageFolderLocation, string outputFolderLocation)
         {
             string htmlTemplate = Properties.Resources.Html_Template;
 
@@ -36,7 +37,7 @@ namespace RegistrationFormGenerator.Library
             htmlDocument.GetElementbyId("RegistrationNo").InnerHtml = data.RegistrationNo;
             htmlDocument.GetElementbyId("DepertmentBengali").InnerHtml = data.DepertmentBengali;
             htmlDocument.GetElementbyId("DepertmentEnglish").InnerHtml = data.DepertmentEnglish;
-            //htmlDocument.GetElementbyId("Image").InnerHtml = data.DepertmentEnglish;
+            htmlDocument.GetElementbyId("Image").SetAttributeValue("src", imageFolderLocation +"\\"+ data.RegistrationNo+".jpg");
             htmlDocument.GetElementbyId("NameBengali").InnerHtml = data.NameBengali;
             htmlDocument.GetElementbyId("NameEnglish").InnerHtml = data.NameEnglish;
             htmlDocument.GetElementbyId("DateOfBirth").InnerHtml = data.DateOfBirth;
@@ -45,7 +46,6 @@ namespace RegistrationFormGenerator.Library
             htmlDocument.GetElementbyId("MotherNameBengali").InnerHtml = data.MotherNameBengali;
             htmlDocument.GetElementbyId("MotherNameEnglish").InnerHtml = data.MotherNameEnglish;
             htmlDocument.GetElementbyId("Session").InnerHtml = data.Session;
-
 
             string html = htmlDocument.DocumentNode.OuterHtml;
 
--- RegistrationFormGenerator/MainWindow.xaml.cs
+++ RegistrationFormGenerator/MainWindow.xaml.cs
@@ -70,7 +70,7 @@ namespace RegistrationFormGenerator
             foreach (ExcelDataRow data in excelDataList)
             {
                 ProgressPdfCreatePercenage.Value += 1/excelDataList.Count*100;  //Progress bar increament
-                ExcelPdfGenerator.GeneratePdf(data, OutputFolderLocation.Text);      //Generate PDF Here
+                ExcelPdfGenerator.GeneratePdf(data, ImageFolderLocation.Text,OutputFolderLocation.Text);      //Generate PDF Here
             }
 
             ResetFields();
